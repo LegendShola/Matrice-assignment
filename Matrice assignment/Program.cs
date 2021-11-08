af59b3c60f2c3245f11fecab401c7dd1d7beff09
Oluwashola Olagbemisoye <shola.mich.7438@gmail.com>
2021-11-08 09:03:45 +0100

    Add project files.

---
+++ Matrice assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Matrice_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose any of the Operation\n1: For Multiplying Matrix\n2: " +
+               "For Finding Matrix Determinant\n3: For Finding Matrix Dot Program");
+            int choice = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (choice == 1)
+            {
+                MatrixMultiplication mul = new MatrixMultiplication();
+                mul.multiplication();
+            }
+            else if (choice == 2)
+            {
+                Determinant mat = new Determinant();
+                mat.Display();
+            }
+            else if (choice == 3)
+            {
+                Dot dot = new Dot();
+                dot.Dots();
+            }
+            else
+            {
+                Console.WriteLine("No Such Choice");
+            }
+        }
+    }
+}
